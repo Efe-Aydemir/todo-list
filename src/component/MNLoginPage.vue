615d6baf36816fc8d751c13fd6166496ed0dda51
Efe Aydemir <delidoluefe42@gmail.com>
2022-09-06 15:07:10 +0300

    Firebase ile kullanıcı kaydı oluşturuldu

--- src/component/MNLoginPage.vue
+++ src/component/MNLoginPage.vue
@@ -4,35 +4,44 @@
         <div class="flex flex-col h-96 w-72 bg-gray-800 rounded-md">
             <div id="username" class="flex flex-col gap-5 pt-5 text-center">
                 <label class="text-xl text-white">Gmail</label>
-                <input type="text" v-model="gmail" name="gmail" id="gmail" class="w-full h-12 border-l-8 border-r-8 transition-all duration-500 bg-black outline-none text-white p-4" :class="namecls" required>
+                <input type="text" v-model="gmail" name="gmail" id="gmail"
+                    class="w-full h-12 border-l-8 border-r-8 transition-all duration-500 bg-black outline-none text-white p-4"
+                    :class="namecls" required>
             </div>
             <div id="password" class="flex flex-col gap-5 pt-5 text-center">
                 <label class="text-xl text-white">Password</label>
-                <input type="password" v-model="nameIn2" name="name" id="name2" class="w-full h-12 border-l-8 border-r-8 transition-all duration-500 bg-black outline-none text-white p-4" :class="namecls2" required>
+                <input type="password" v-model="nameIn2" name="name" id="name2"
+                    class="w-full h-12 border-l-8 border-r-8 transition-all duration-500 bg-black outline-none text-white p-4"
+                    :class="namecls2" required>
             </div>
-                <button class="border-2 border-white transition-all duration-300 rounded-md text-white hover:bg-black hover:border-opacity-0 w-full mt-20 p-3 bg-transparent" @click="login">Log in</button>
+            <button
+                class="border-2 border-white transition-all duration-300 rounded-md text-white hover:bg-black hover:border-opacity-0 w-full mt-20 p-3 bg-transparent"
+                @click="login">Log in</button>
         </div>
     </div>
 </template>
 
 <script setup>
-import {ref, watch} from 'vue'
+import { ref, watch } from 'vue'
+import { createUserWithEmailAndPassword , getAuth} from 'firebase/auth'
 import router from '@/router'
+
 const gmail = ref('')
 const namecls = ref('border-red-600')
 const nameIn2 = ref('')
 const namecls2 = ref('border-red-600')
+const auth = getAuth()
 
 watch(gmail, () => {
     if (gmail.value.length > 0) {
-    namecls.value = 'border-green-600'
-} else {
-    namecls.value = 'border-red-600'
-}
+        namecls.value = 'border-green-600'
+    } else {
+        namecls.value = 'border-red-600'
+    }
 })
 
 watch(nameIn2, () => {
-    if(nameIn2.value.length > 0) {
+    if (nameIn2.value.length > 0) {
         namecls2.value = 'border-green-600'
     } else {
         namecls2.value = 'border-red-600'
@@ -40,14 +49,22 @@ watch(nameIn2, () => {
 })
 
 
+
 function login() {
-    if (gmail.value.length <= 0 && nameIn2.value.length <= 0){
+    if (gmail.value.length <= 0 && nameIn2.value.length <= 0) {
         alert('Lütfen Gerekli Yerleri Doldurunuz')
-    } else if(gmail.value.length >= 0 && nameIn2.value.length >= 0) {
-        alert('Giriş Sağlandı')
-        localStorage.setItem('gmail' , gmail.value)
+    } else if (gmail.value.length >= 0 && nameIn2.value.length >= 0) {
+        localStorage.setItem('gmail', gmail.value)
         localStorage.setItem('passw', nameIn2.value)
-        router.push('/todo')
+        createUserWithEmailAndPassword(auth , gmail.value, nameIn2.value)
+            .then(() => {
+                router.push('/todo')
+            })
+            .catch(error => {
+                alert(error)
+            })
+
+
         gmail.value = ""
         nameIn2.value = ""
     } else {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import './index.css'
 import YartuUIKit from "@yartu/ui-kit";
 import router from '@/router'
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2n3u-CGq8DnusEwQZ-AZs0nw4yOruVFc",
+    authDomain: "todolist-4b736.firebaseapp.com",
+    projectId: "todolist-4b736",
+    storageBucket: "todolist-4b736.appspot.com",
+    messagingSenderId: "697822777756",
+    appId: "1:697822777756:web:facebc131d4251fa9d7859",
+    measurementId: "G-ZG238ZLKS6"
+  };
+
+firebase.initializeApp(firebaseConfig)
 
 
 createApp(App).use(router).use(YartuUIKit).mount('#app')
